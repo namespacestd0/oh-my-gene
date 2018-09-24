bbf1e84738f356333b9b3891d3dd58b3a325fea3
Jerry Zhou <xiz170@ucsd.edu>
2018-09-23 22:17:05 -0700

    Update Entry Point Using Wildcard Character

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ require('./routes/gene')(app);
 require('./routes/search')(app);
 
 // entry point
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
